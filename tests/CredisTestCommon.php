54cda594588f795f2506bff07582622247b0d8e0
Colin Mollenhour <colin@mollenhour.com>
2017-10-05 15:43:58 -0400

    Update CredisTestCommon.php

--- tests/CredisTestCommon.php
+++ tests/CredisTestCommon.php
@@ -1,7 +1,7 @@
 <?php
 // backward compatibility (https://stackoverflow.com/a/42828632/187780)
 if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
-    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
 }
 
 class CredisTestCommon extends \PHPUnit\Framework\TestCase
